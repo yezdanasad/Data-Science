58a0581581d9da09742ef6e8063c68e765707982
Asad <119559809+yezdanasad@users.noreply.github.com>
2024-02-14 12:38:27 +0000

    Add files via upload
    Learning to train neural networks, created a training data set and used it to train a neural network to generate solutions to the Black-Schole-Merton equation for option pricing.
    Best set of results showed an MSE of +4 cents off of actual price across approx. 7.5 million data points.

---
+++ BSMNN - Data set generate.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import keras
+import keras.backend as K
+
+
+from sklearn.model_selection import ParameterGrid
+from py_vollib import black_scholes_merton as bsm
+from progressbar import ProgressBar
+from scipy.stats import gamma
+from scipy.stats import beta
+from scipy.stats import uniform
+from keras.models import Model
+from keras.layers import Input, Dense
+from sklearn.model_selection import train_test_split
+# S (spot price)
+def thisS (q):
+ return gamma.ppf(q, a = 100, scale = 1)
+# K (strike price)
+# uniform (lower = 50, upper = 200)
+def thisK (q):
+ return uniform.ppf(q, 50, 200)
+# (interest rate)
+# uniform (lower = 0.01, upper = 0.18)
+def thisR (q):
+ return uniform.ppf(q, 0.01, 0.18)
+# D (dividend)
+# uniform (lower = 0.01, upper = 0.18)
+def thisD (q):
+ return uniform.ppf(q, 0.01, 0.18)
+17
+# t (time-to-maturity)
+# t will be 3, 6, 9, 12 months for all examples (0.25, 0.5, 0.75, 1 year)
+# sigma (volatility)
+# beta (add small amount so volatility cannot be zero)
+def thisSigma (q):
+ return (beta.ppf(q, a = 2, b = 5) + 0.001)
+num_increment = 12
+percentiles = pd.Series(np.linspace(0, 0.99, num_increment))
+S = percentiles.apply(thisS).tolist()
+K = percentiles.apply(thisK).tolist()
+q = percentiles.apply(thisD).tolist()
+t = np.array([.25, .5, .75, 1])
+r = percentiles.apply(thisR).tolist()
+
+S.pop(0)
+sigma = percentiles.apply(thisSigma).tolist()
+param_grid = {'S': S, 'K' : K, 'q' : q, 't' : t, 'r' : r, 'sigma' : sigma}
+grid = ParameterGrid(param_grid)
+
+
+pbar = ProgressBar()
+fullDF = pd.DataFrame(columns =['K','S','q','r','sigma','t','price'])
+prices = []
+
+for params in pbar(grid):
+ price = bsm.black_scholes_merton(flag='p', S=params['S'], K=params['K'], q=params['q'], t=params['t'], r=params['r'], sigma=params['sigma'])
+ price = round(price,2)
+ if price < 0.01:
+  price = 0.0
+  params['price'] = price
+ else:
+  params['price'] = price
+ temp_df = pd.DataFrame([params])
+ fullDF = pd.concat([fullDF, temp_df], ignore_index=True)
+
+
+
+
+# output to csv
+fullDF.to_csv('C:\\Users\\yezda\\OneDrive\\Python Virtual Environments\\BlackScholeML\\BSMNNvenv\\dataFullV2.csv', index = False)
+print('csv has been saved to venv file!')
---
+++ BSMNN-Neunet.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import keras 
+import keras.backend as K
+import tensorflow
+
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import ParameterGrid
+from py_vollib import black_scholes_merton as bsm
+from progressbar import ProgressBar
+from scipy.stats import gamma
+from scipy.stats import beta
+from scipy.stats import uniform
+from keras.optimizers import Adagrad
+from keras.models import Model
+from keras.layers import Input, Dense
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import LearningRateScheduler
+
+
+
+path = 'file:///C:/Users/yezda/Downloads/dataFullV2.csv'
+df_training = pd.read_csv(path)
+
+
+inputs = df_training[['K','S','q','r','sigma','t']]
+outputs = df_training[['price']]
+
+# I had the thought that the reason that the accuracy sucks balls when training is because my training set might have folating point arithmetic
+#problems and the NN output doesnt so, accuracy is fucking garbage while cost goes dowm, the idea to fix this is to use StandardScaler() and
+#hope thatnormalised data will deal with the wackiness of floating point arithmtic and things will worl better.
+'''scaler = StandardScaler()
+
+# Fit the scaler to your DataFrame and transform it
+inputs_scaled = pd.DataFrame(scaler.fit_transform(inputs), columns=inputs.columns)
+outputs_scaled = pd.DataFrame(scaler.fit_transform(outputs), columns=outputs.columns)
+'''
+
+x_train,x_test,y_train,y_test = train_test_split(inputs, outputs, test_size=0.1, random_state=7)
+
+def baseline_model():
+    # create model
+    i = Input(shape=(6,))
+    x = Dense(10, activation='relu')(i)
+    y = Dense(10, activation='relu')(x)
+    o = Dense(1)(y)
+    model = Model(i, o)
+    # Use SGD optimizer with specified learning rate and momentum
+    '''optimizer = Adagrad(learning_rate=0.001,initial_accumulator_value=0.1,epsilon=1e-06,weight_decay=None,clipnorm=None,clipvalue=None,global_clipnorm=None,use_ema=False,ema_momentum=0.99,ema_overwrite_frequency=None,jit_compile=True,name="Adagrad")'''
+
+    model.compile(loss="mse", optimizer='adam', metrics=['accuracy'])
+    return model
+model_full = baseline_model()
+'''
+def learning_rate_schedule(epoch):
+    initial_lr = 0.001  # Initial learning rate
+    decay = 0.95  # Learning rate decay factor
+    return initial_lr * (decay ** epoch)
+
+# Create the LearningRateScheduler callback
+lr_scheduler = LearningRateScheduler(learning_rate_schedule)
+'''
+percentage_differences = []
+history_full = model_full.fit(x_train, y_train, epochs=50, batch_size=64)
+test_output = model_full.predict(x_test)
+
+print('______________________________________EVALUATION METRICS______________________________________')
+print('______________________________________________________________________________________________')
+print (f'mean squared error for option pricing model on out of sample test is {mean_squared_error(y_test,test_output)}')
+
+
